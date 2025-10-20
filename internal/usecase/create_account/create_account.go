05c54ae860952b99dc95f73e61b7de079ca7b97e
Rafael Freitas <raffreitasx@gmail.com>
2025-10-20 11:15:41 -0300

    Implement CreateAccount use case and associated tests

---
+++ internal/usecase/create_account/create_account.go
@@ -0,0 +1,47 @@
+package createaccount
+
+import (
+	"github.com/raffreitas/fc-ms-wallet/internal/entity"
+	"github.com/raffreitas/fc-ms-wallet/internal/gateway"
+)
+
+type CreateAccountInputDTO struct {
+	ClientID string
+}
+
+type CreateAccountOutputDTO struct {
+	ID string
+}
+
+type CreateAccountUseCase struct {
+	AccountGateway gateway.AccountGateway
+	ClientGateway  gateway.ClientGateway
+}
+
+func NewCreateAccountUseCase(accountGateway gateway.AccountGateway, clientGateway gateway.ClientGateway) *CreateAccountUseCase {
+	return &CreateAccountUseCase{
+		AccountGateway: accountGateway,
+		ClientGateway:  clientGateway,
+	}
+}
+
+func (uc *CreateAccountUseCase) Execute(input CreateAccountInputDTO) (*CreateAccountOutputDTO, error) {
+	client, err := uc.ClientGateway.Get(input.ClientID)
+	if err != nil {
+		return nil, err
+	}
+
+	account, err := entity.NewAccount(client)
+	if err != nil {
+		return nil, err
+	}
+
+	err = uc.AccountGateway.Save(account)
+	if err != nil {
+		return nil, err
+	}
+
+	return &CreateAccountOutputDTO{
+		ID: account.ID,
+	}, nil
+}
